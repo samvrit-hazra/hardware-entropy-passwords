77e7768f7fdfd7ce7eade257f27cf12124c9c355
Samvrit Hazra <samvrit@zohomail.in>
2025-12-12 20:12:49 +0530

    Add generator.h

---
+++ include/generator.h
@@ -0,0 +1,10 @@
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+char *generate_password(int length);
+char get_lower_case();
+char get_upper_case();
+char get_digit();
+char get_special_char();
+
+#endif
